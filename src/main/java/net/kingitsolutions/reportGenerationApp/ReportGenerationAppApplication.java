e17eebff5e1c5c35fb2b193ce007fae74bec29ef
azharmo22350 <azharmohammed22350@gmail.com>
2024-06-28 14:36:22 +0530

    Initial project setup

---
+++ src/main/java/net/kingitsolutions/reportGenerationApp/ReportGenerationAppApplication.java
@@ -0,0 +1,13 @@
+package net.kingitsolutions.reportGenerationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportGenerationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportGenerationAppApplication.class, args);
+	}
+
+}
